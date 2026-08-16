5ee8c18abb7a82ea5c9d96c1ac52d79e250f0cd6
SOUMYA NAYAK <soumyanayakk08@gmail.com>
2026-08-16 15:12:16 +0530

    Time: 0 ms (100%), Space: 35.8 MB (29.64%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total =0;
+        for(int num : nums){
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        for(int i =0;i<nums.size();i++){
+             total -=nums[i];
+
+             if(leftSum == total){
+                return i;
+             }
+             leftSum += nums[i];
+        }
+        return -1;
+    }
+};
