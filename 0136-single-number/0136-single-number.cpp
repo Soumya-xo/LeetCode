cf2d695413966c939cc64a8a0dc547d09178ca57
SOUMYA NAYAK <soumyanayakk08@gmail.com>
2026-08-16 12:15:27 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.3%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>freq;
+       int ans = 0;
 
-        for(int num : nums){
-            if(freq.find(num)!=freq.end()){
-                freq[num]--;
-            }else{
-                freq[num]++;
-            }
-        }
-        for(auto it : freq){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
+       for(int num : nums){
+        ans^=num;
+       }
+       return ans;
     }
 };
