5be036dd58b8084925fbd7452d4605f89f81eee8
SOUMYA NAYAK <soumyanayakk08@gmail.com>
2026-09-02 22:49:57 +0530

    Time: 5 ms (20.07%), Space: 8.7 MB (20.56%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int rev=0;
+        while(n!=0){
+            int rem = n%10;
+            n/=10;
+
+            if(rev> INT_MAX/10  ||  rev==INT_MAX/10 && rem>7 )return 0;
+
+            if(rev< INT_MIN/10 || rev == INT_MIN/10 && rem < -8)return 0;
+
+            rev = rev*10 +rem;
+        }
+        return rev;
+    }
+};
