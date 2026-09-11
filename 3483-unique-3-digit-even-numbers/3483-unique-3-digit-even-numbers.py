017b557d00ef07cb19ea5d5e3b47964ba16b0d7b
SOUMYA NAYAK <soumyanayakk08@gmail.com>
2026-09-11 18:46:01 +0530

    Time: 6 ms (97.8%), Space: 19.3 MB (72.53%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -0,0 +1,30 @@
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        freq = [0] * 10
+
+        for d in digits:
+            freq[d] += 1
+
+        ans = 0
+
+        for a in range(1, 10):
+            if freq[a] == 0:
+                continue
+
+            freq[a] -= 1
+
+            for b in range(10):
+                if freq[b] == 0:
+                    continue
+
+                freq[b] -= 1
+
+                for c in range(0, 10, 2):
+                    if freq[c] > 0:
+                        ans += 1
+
+                freq[b] += 1
+
+            freq[a] += 1
+
+        return ans
